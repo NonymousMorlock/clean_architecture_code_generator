45110a4f6f1623b97f29b52cd682b1b51c06c441
NonymousMorlock <agamspaul@gmail.com>
2025-12-21 09:03:05 +0000

    🗑 Chore(example): Remove unused `typedefs.dart` and `usecase.dart` files

--- example/lib/typedefs.dart
+++
@@ -1,5 +0,0 @@
-import 'package:example/repo_gen.dart';
-
-typedef DataMap = Map<String, dynamic>;
-typedef ResultFuture<T> = Future<Either<Failure, T>>;
-typedef ResultStream<T> = Stream<Either<Failure, T>>;
