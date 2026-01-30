de1b939390a12ed0851c3c57628074e9a9cd7608
NonymousMorlock <agamspaul@gmail.com>
2026-01-30 19:04:41 +0000

    🧹 Refactor(generators): Remove `oneMemberAbstractHandler` from `Utils`
    This commit removes the `oneMemberAbstractHandler` static method from the `Utils` class. This utility, which was used to inject `one_member_abstracts` lint suppression comments into generated files, is no longer needed.

--- generators/lib/core/utils/utils.dart
+++ generators/lib/core/utils/utils.dart
@@ -7,26 +7,6 @@ sealed class Utils {
   /// Private constructor to prevent instantiation.
   const Utils();
 
-  /// Adds a lint ignore comment for single-method abstract classes.
-  ///
-  /// When an interface has only one method, adds an ignore comment
-  /// to suppress the `one_member_abstracts` lint warning.
-  static void oneMemberAbstractHandler({
-    required StringBuffer buffer,
-    required int methodLength,
-  }) {
-    if (methodLength < 2) {
-      buffer.writeAll(
-        [
-          '// I need this class to be an interface.',
-          '// ignore_for_file: one_member_abstracts',
-          '',
-        ],
-        '\n',
-      );
-    }
-  }
-
   /// Discovers entity candidates used in a repository method.
   ///
   /// Scans the return type and parameters of the method to identify
